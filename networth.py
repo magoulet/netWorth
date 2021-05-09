eece21be0da049fe3b7bcd41068777cf80bd04e6
magoulet <magoulet@gmail.com>
2021-05-09 11:02:25 -0700

    refactored, pulling data from database and using fbprophet to make predictions

--- networth.py
+++ networth.py
@@ -1,31 +1,81 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import chart_studio
-import chart_studio.plotly as py
-import os
+from fbprophet import Prophet
+import datetime as dt
+import matplotlib.pyplot as plt
 import pandas as pd
-import plotly.express as px
-import yaml
+# import yaml
+from dbconnect import connection
 
-conf = yaml.load(open('config.yaml'),Loader=yaml.FullLoader)
-
-username = conf['username']
-api_key = conf['plotlyApiKey']
-input_file = conf['inputFile']
-
-chart_studio.tools.set_credentials_file(username=username, api_key=api_key)
+plt.style.use('ggplot')
 
 
-df = pd.read_csv(input_file, 
-                 parse_dates = ['Date'], 
-                 index_col='Date')
+def getData():
+    cur, conn = connection("portfolio")
+    resultValue = cur.execute("SELECT * FROM networth")
+    if resultValue > 0:
+        data = cur.fetchall()
+        df = pd.DataFrame(data)
+        return df
+    else:
+        return "Database is empty!"
 
-fig = px.area(df,
-    title='Net Worth (CAD)',
-    labels={'variable':'Net Worth (CAD)',
-            'value': 'Net Worth (CAD)'})
-fig.update_layout(showlegend=False)
-             
-#fig.show()
-py.plot(fig, filename = 'net_worth', auto_open=False)
+
+def predict(df):
+    df.columns = ['ds', 'y']
+    prophet = Prophet(daily_seasonality=False, weekly_seasonality=False)
+
+    # fit data to model
+    prophet.fit(df)
+
+    # build future dataframe for 5 years
+    future = prophet.make_future_dataframe(periods=5*12, freq='M')
+
+    # make predictions
+    forecast = prophet.predict(future)
+
+    # plot forecasts
+    prophet.plot(forecast, xlabel='Date', ylabel='Net Worth')
+    plt.title('Net Worth Forecast')
+
+    # tell us more about the forecast
+    # prophet.plot_components(forecast)
+
+    plt.show(block=False)
+
+    return forecast
+
+
+def exploreForecast(forecast, df):
+    predict = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+    predict.columns = ['date', 'net worth', 'upper bound', 'lower bound']
+    output = predict.loc[predict['date'] > dt.datetime.today(),
+                         ['date', 'net worth']][::6]
+
+    print('Net worth targets (CAD) : ')
+    for index, row in output.iterrows():
+        print('{:%Y-%m-%d}'.format(row['date']),
+              ': $'+'{:,.0f}'.format(row['net worth']))
+
+    print('\rCurrent Net Worth: ' + '$' + '{:,.0f}'.format(df.iloc[-1, 1]))
+
+    return None
+
+
+if __name__ == '__main__':
+    df = getData()
+    df.columns = ['date', 'netWorth']
+    df['date'] = df['date'].astype('datetime64[ns]')
+    df['netWorth'] = df['netWorth'].astype('float')
+
+    # plt.figure(figsize=(8, 4))
+    # plt.grid(linestyle='-.')
+    # plt.plot(df['date'], df['netWorth'])
+    # plt.show(block=False)
+
+    forecast = predict(df)
+
+    exploreForecast(forecast, df)
+
+    plt.show()
