85575a3d35f7aea660295bc1da07cba363bb908b
magoulet <magoulet@gmail.com>
2023-04-25 22:24:01 -0700

    Refactored

--- dbconnect.py
+++ dbconnect.py
@@ -1,4 +1,4 @@
-import MySQLdb
+import pymysql
 import os
 import yaml
 
@@ -8,7 +8,7 @@ curPath = os.path.dirname(os.path.abspath(__file__))
 conf = yaml.load(open(curPath+'/db.yaml'),Loader=yaml.FullLoader)
 
 def connection(dbName):
-    conn = MySQLdb.connect(host=conf['mysql_host'],
+    conn = pymysql.connect(host=conf['mysql_host'],
                            user = conf['mysql_user'],
                            passwd = conf['mysql_password'],
                            db = dbName)
--- networth.py
+++ networth.py
@@ -5,21 +5,23 @@ from prophet import Prophet
 import datetime as dt
 import matplotlib.pyplot as plt
 import pandas as pd
-from dbconnect import connection
+# import yaml
+import pymysql
 
 plt.style.use('ggplot')
 
 
-def getData():
-    cur, conn = connection("portfolio")
-    resultValue = cur.execute("SELECT * FROM networth WHERE date > (DATE_SUB(CURDATE(), INTERVAL 2 YEAR))")
-    if resultValue > 0:
-        data = cur.fetchall()
-        df = pd.DataFrame(data)
-        return df
-    else:
-        return "Database is empty!"
+def get_data():
+    # Connect to the database
+    db = pymysql.connect(host="raspberrypi1", port=3306, user="admin", password="password", database="portfolio")
 
+    # Fetch data from the networth table
+    query = "SELECT date, netWorthUsd FROM networth"
+    df = pd.read_sql(query, db)
+    # Convert the 'date' column to datetime and set as index
+    # df["date"] = pd.to_datetime(df["date"])
+    # df.set_index("date", inplace=True)
+    return df
 
 def predict(df):
     df.columns = ['ds', 'y']
@@ -49,13 +51,16 @@ def predict(df):
 def exploreForecast(forecast, df):
     predict = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
     predict.columns = ['date', 'net worth', 'upper bound', 'lower bound']
-    output = predict.loc[predict['date'] > dt.datetime.today(),
-                         ['date', 'net worth']][::6]
 
-    print('Net worth targets (CAD) : ')
-    for index, row in output.iterrows():
-        print('{:%Y-%m-%d}'.format(row['date']),
-              ': $'+'{:,.0f}'.format(row['net worth']))
+    predict = predict.loc[predict['date'] > dt.datetime.today(),['date', 'net worth']]
+
+    predict.set_index('date', inplace=True)
+
+    target_dates = predict.index[(predict.index.month == 6) & (predict.index.day == 30) | (predict.index.month == 12) & (predict.index.day == 31)]
+
+    for target_date in target_dates:
+        net_worth_target = predict.loc[target_date]["net worth"]
+        print(f"{target_date.strftime('%Y-%m-%d')}: ${net_worth_target:,.0f}")
 
     print('\rCurrent Net Worth: ' + '$' + '{:,.0f}'.format(df.iloc[-1, 1]))
 
@@ -63,14 +68,11 @@ def exploreForecast(forecast, df):
 
 
 if __name__ == '__main__':
-    df = getData()
-    df.columns = ['date', 'netWorth']
-    df['date'] = df['date'].astype('datetime64[ns]')
-    df['netWorth'] = df['netWorth'].astype('float')
+    df = get_data()
 
-    plt.figure(figsize=(8, 4))
-    plt.grid(linestyle='-.')
-    plt.plot(df['date'], df['netWorth'])
+    # Plot the results
+    plt.plot(df.date, df['netWorthUsd'], label='Actual')
+    plt.legend()
     plt.show(block=False)
 
     forecast = predict(df)
